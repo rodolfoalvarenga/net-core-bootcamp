19673cfdf3100518f96b8754b458324019be3fe5
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-03-01 19:19:34 -0300

    camada Application completa

---
+++ DevFreela.Application/Services/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using DevFreela.Application.InputModels;
+using DevFreela.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFreela.Application.Services.Interfaces
+{
+    public interface IUserService
+    {
+        UserViewModel GetUser(int id);
+        int Create(CreateUserInputModel inputModel);
+    }
+}
