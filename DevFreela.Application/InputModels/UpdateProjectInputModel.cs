14865c993e8949c337a125522a823200b0f11581
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-03-01 19:02:38 -0300

    camada Application parte 6

--- DevFreela.Application/InputModels/UpdateProjectInputModel.cs
+++ DevFreela.Application/InputModels/UpdateProjectInputModel.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DevFreela.Application.InputModels
+﻿namespace DevFreela.Application.InputModels
 {
     public class UpdateProjectInputModel
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal TotalCost { get; set; }
     }
 }
---
+++ DevFreela.Application/ViewModels/SkillViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFreela.Application.ViewModels
+{
+    public class SkillViewModel
+    {
+        public SkillViewModel(int id, string description)
+        {
+            Id = id;
+            Description = description;
+        }
+
+        public int Id { get; private set; }
+        public string Description { get; private set; }
+    }
+}
