43a93c480165e45e98cb57e14e7947d5f5a615dd
RodolfoAlvarenga <rodolfo.l.alvarenga@gmail.com>
2021-03-01 18:09:09 -0300

    camada Application parte 2

---
+++ DevFreela.Application/InputModels/CreateCommentInputModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFreela.Application.InputModels
+{
+    public class CreateCommentInputModel
+    {
+    }
+}
---
+++ DevFreela.Application/ViewModels/ProjectDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFreela.Application.ViewModels
+{
+    public class ProjectDetailsViewModel
+    {
+    }
+}
